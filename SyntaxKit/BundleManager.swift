1b6961d60cd16b37e11c787f0cef20dabe86387f
Alexander Hedges <alehed@users.noreply.github.com>
2016-07-13 19:09:04 +0200

    Document BundleManager

--- SyntaxKit/BundleManager.swift
+++ SyntaxKit/BundleManager.swift
@@ -2,6 +2,12 @@
 //  BundleManager.swift
 //  SyntaxKit
 //
+//  Used to get access to SyntaxKit representations of TextMate bundle files. 
+//  This class is used as a gateway for both internal and external use. 
+//  Alternatively a global instace can be used for convenience. It is 
+//  initialized with a callback that tells the bundle manager where to find the
+//  files.
+//
 //  Created by Alexander Hedges on 15/02/16.
 //  Copyright © 2016 Alexander Hedges. All rights reserved.
 //
@@ -10,11 +16,22 @@ public class BundleManager {
     
     // MARK: - Types
     
+    /// Given an identifier of a grammar file and the format returns a url to the resource.
+    /// 
+    /// - parameter identifier: The identifier of the file. Used to map it to 
+    ///                         the name of the file.
+    /// - parameter isLanguage: Whether the requested file stores a language 
+    ///                         (.tmLanguage)
+    /// - returns:  A URL pointing to the resource, if found
     public typealias BundleLocationCallback = (identifier: String, isLanguage: Bool) -> (NSURL?)
     
     
     // MARK: - Properties
     
+    /// You probably want to leave the languageCaching property set to true. 
+    ///
+    /// - note: Setting it to false will not invalidate or purge the cache. This
+    ///         has to be done separately using clearLanguageCache.
     public var languageCaching = true
     
     public static var defaultManager: BundleManager?
@@ -27,6 +44,11 @@ public class BundleManager {
     
     // MARK: - Initializers
     
+    /// Used to initialize the default manager. Unless this is called the
+    //  defaultManager property will be set to nil.
+    ///
+    /// - parameter callback:   The callback used to find the location of the
+    ///                         textmate files.
     public class func initializeDefaultManagerWithLocationCallback(callback: BundleLocationCallback) {
         if defaultManager == nil {
             defaultManager = BundleManager(callback: callback)
@@ -48,7 +70,7 @@ public class BundleManager {
         }
         
         self.dependencies = []
-        var language = self.getUnvalidatedLanguageWithIdentifier(identifier)!
+        var language = self.getRawLanguageWithIdentifier(identifier)!
         language.validateWithHelperLanguages(self.dependencies)
         
         if languageCaching {
@@ -74,6 +96,7 @@ public class BundleManager {
         return newTheme
     }
     
+    /// Clears the language cache. Use if low on memory.
     public func clearLanguageCache() {
         self.cachedLanguages = [:]
     }
@@ -81,7 +104,9 @@ public class BundleManager {
     
     // MARK: - Internal Interface
     
-    func getUnvalidatedLanguageWithIdentifier(identifier: String) -> Language? {
+    /// - parameter identifier: The identifier of the requested language.
+    /// - returns:  The Language with unresolved extenal references, if found
+    func getRawLanguageWithIdentifier(identifier: String) -> Language? {
         let indexOfStoredLanguage = self.dependencies.indexOf{ (lang: Language) in lang.scopeName == identifier }
         
         if indexOfStoredLanguage != nil {
