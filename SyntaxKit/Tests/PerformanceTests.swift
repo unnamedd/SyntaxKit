020995f66f01f0ccbe979ca4762952b5c70948d8
Alexander Hedges <alehed@users.noreply.github.com>
2017-01-05 22:50:52 +0100

    add performance test to measure #20

---
+++ SyntaxKit/Tests/PerformanceTests.swift
@@ -0,0 +1,43 @@
+//
+//  PerformanceTests.swift
+//  SyntaxKit
+//
+//  Created by Alexander Hedges on 05.01.17.
+//  Copyright © 2017 Sam Soffes. All rights reserved.
+//
+
+import XCTest
+import SyntaxKit
+
+class PerformanceTests: XCTestCase {
+
+    // MARK: - Properties
+
+    let manager = getBundleManager()
+    var parser: AttributedParser!
+    
+    override func setUp() {
+        super.setUp()
+        let latex = manager.language(withIdentifier: "source.Latex")!
+        let solarized = manager.theme(withIdentifier: "Solarized")!
+        parser = AttributedParser(language: latex, theme: solarized)
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testLongTexFilePerformance() {
+        let input = fixture("textest.tex", "txt")
+        self.measure {
+            _ = self.parser.attributedString(for: input)
+        }
+    }
+    
+}
